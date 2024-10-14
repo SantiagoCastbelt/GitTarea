f19525422a191da807b457926c4f83ef85c58af0
SantiagoCast <santiago.castellon@cecar.edu.co>
2024-10-14 16:05:43 -0500

    Proyecto Git

---
+++ TareaGit/TareaGit/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TareaGit
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Santiago(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new santiagoPage());
+        }
+    }
+
+}
